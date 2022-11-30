e6d1ef23d6bb5324406259ee101e37df2693381a
rokugatsua <rokugatsua@outlook.co.id>
2022-11-30 12:07:54 +0700

    update stage

---
+++ scenes/stage/World.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public class World : Node2D 
+{
+    OpenSimplexNoise noise;
+    public override void _Ready()
+    {
+    }
+
+    private void getTexture()
+    {
+    }
+}
