a84078b33504432d5851a88403d0ad9e11726d87
rokugatsua <rokugatsua@outlook.co.id>
2022-12-11 10:26:17 +0700

    add stats

--- components/stats/Stats.cs
+++ components/stats/Stats.cs
@@ -1,7 +1,27 @@
 using Godot;
 using System;
-
-public class stats : Resource
+[Tool]
+public class Stats : Resource
 {
+    [Signal] delegate void ChangedHealth(int health);
+    [Signal] delegate void KnockedOut();
+    private int _health = 0;
+    public int Health {get{return _health;} private set{}}
+    [Export] public int HealthPoint{get; set;}
+    [Export] public int Attack{get; set;}
+    [Export] public int AttackSpeed{get; set;}
+    [Export] public int Critical{get; set;}
+    [Export] public int PickupRange{get; set;}
+    [Export] public int MovementSpeed{get; set;}
 
+    public void SetHealth(int health)
+    {
+        if (!(health >= 0 && health <= HealthPoint)) return;
+
+        _health = health;
+        EmitSignal(nameof(ChangedHealth), _health);
+        if (_health == 0) {
+            EmitSignal(nameof(KnockedOut));
+        }
+    }
 }
